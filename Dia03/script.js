584e94cd17300056d77f05f76f1cd55a9c171377
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-12 23:24:10 -0300

    Dia03

---
+++ Dia03/script.js
@@ -0,0 +1,20 @@
+// Variables
+// var ou let (let é o formato novo de se declarar)
+// ambos podem ser manipuladas
+
+/* 
+camelCase -> Boas práticas para nomes de variáveis
+    primeira palavra em minúsculo
+    se tiver mais palavras iniciam em maiúsculas
+*/
+
+let firstName = 'Wellington'
+firstName = 'Welly'
+let lastName = 'Sousa'
+let age = 63
+console.log(firstName, lastName, age)
+
+// const
+
+const calTax = 0.7 // Não pode ser alterada e nem inicializada com nada
+console.log(calTax)
