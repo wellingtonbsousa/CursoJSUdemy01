dab6169c86ce8c44a3e3eabb916cdcbc0ace7759
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-13 17:45:02 -0300

    Dia06a

---
+++ Dia06/script.js
@@ -0,0 +1,27 @@
+// Operadores Aritméticos
+
+let total = 6 + 2 // Adição
+console.log(total)
+
+total = 6 - 2 // Subtração
+console.log(total)
+
+total = 6 * 2 // Multiplicação
+console.log(total)
+
+total = 6 / 2 // Divisão
+console.log(total)
+
+total = 5 % 3 // Resto da Divisão
+console.log(total)
+
+total = 5
+total++ // Incremento
+console.log(total)
+
+total = 5
+total-- // Decremento
+console.log(total)
+
+total = 2 ** 3 // Exponencial
+console.log(total)
