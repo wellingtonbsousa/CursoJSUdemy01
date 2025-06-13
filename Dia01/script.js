c95adf7e9827240ae7ab98d0529053cd9c3a52ce
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-12 22:52:31 -0300

    Dia 01

---
+++ Dia01/script.js
@@ -0,0 +1,14 @@
+console.log('Wellington')
+console.warn('Erro 300')
+console.error('Erro 550')
+
+console.group('App')
+console.log('Wellington')
+console.warn('Erro 300')
+console.error('Erro 550')
+console.groupEnd
+
+/* Atalhos:
+    Alt + Shift + Seta para Baixo -> Duplica a linha acima
+    
+*/
