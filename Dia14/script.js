a1064cad56b3b3323a7950bddeff4edb4d5323d0
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-18 17:46:21 -0300

    Dia 14

--- Dia14/script.js
+++ Dia14/script.js
@@ -1,31 +1,84 @@
 //=========================
-// If e Else
+// AND
 //=========================
-let hour = 19
 
-if (hour <= 12) {
-    console.log('Bom dia!')
-} else if (hour <=18) {
-    console.log('Boa tarde!')
+let idadeParticipante = 19
+let registroOnLine = false
+
+// if (idadeParticipante >= 18 && registroOnLine == true) {
+if (idadeParticipante >= 18 && registroOnLine) {
+    console.log('Bem vindo ao evento!')
 } else {
-    console.log('Boa noite!')
+    console.log('Precisa ser maior de 18 e fazer o registro online!')
 }
 
-// Shorthand do if (não precisa das chaves)
-if (hour <= 12) console.log('Bom dia!')
-else if (hour <=18) console.log('Boa tarde!')
-else console.log('Boa noite!')
+//=========================
+// OR
+//=========================
+let estudante = false
+let cupom = false
 
-// Desafio (pontuação):
-// se for 90 ou mais exibir "Excelente"
-// se for 75 ou mais (menor que 90) exibir "Muito bom!"
-// se for inferior a 75 exibir "Você pode melhorar"
-
-let pontuacao = 70
-if (pontuacao >=90) {
-    console.log('Excelente!')
-} else if (pontuacao >=75) {
-    console.log('Muito bom!')
+if (estudante || cupom) {
+    console.log('Você tem desconto!')
 } else {
-    console.log('Voce pode melhorar')
+    console.log('Sem desconto!')
+}
+
+//=========================
+// AND  e OR
+//=========================
+// Desconto para famílias maiores que 4 e que venham terça ou quarta
+let quantidade = 5
+let dia = 'Quinta'
+
+if (quantidade > 4 && dia === 'Terça' || dia === 'Quarta') {
+    // === -> identico
+    console.log('Conta com desconto')
+} else {
+    console.log('Conta sem desconto')
+}
+
+//=============================
+// Nesting do if (Aninhamento)
+//=============================
+// nivel = true ou false
+//  >= 90 -> Ouro
+//  >= 75 -> Prata
+//  < 75  -> Bronze
+// nivel = false
+//  voce tem que terminar o nivel primeiro
+let nivel = true
+let pontos = 60
+
+if (nivel) {
+    if (pontos >= 90) {
+        console.log('Medalha de Ouro!')
+    } else if (pontos >= 75) {
+        console.log('Medalha de Prata!')
+    } else {
+        console.log('Medalha de bronze!')
+    }
+} else {
+    console.log('Você tem que terminar o nível primeiro!')
+}
+
+//=============================
+// Desafio - Acesso ao sistema
+//=============================
+let usuarioValido = true
+let senhaCorreta = true
+let temPermissao = true
+
+if (usuarioValido) {
+    if (senhaCorreta) {
+        if (temPermissao) {
+            console.log('Acesso permitido!')
+        } else {
+            console.log('Acesso negado. Usuário sem permissão!')
+        }
+    } else {
+        console.log('Senha incorreta. Tente novamente!')
+    }
+} else {
+    console.log('Usuário não encontrado!')
 }
